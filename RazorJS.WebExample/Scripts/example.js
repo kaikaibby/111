e06cbf229f9f25c6eefe471d3cc7b71409a818d9
John Katsiotis <dj7solid@gmail.com>
2012-01-18 22:26:45 +0200

    Minor update

--- RazorJS.WebExample/Scripts/example.js
+++ RazorJS.WebExample/Scripts/example.js
@@ -1,4 +1,6 @@
-﻿var s = 'Hello at @DateTime.Now \n';
+﻿@using System.IO;
+
+var s = 'Hello at @DateTime.Now \n @File.ReadAllText(System.Web.Hosting.HostingEnvironment.MapPath("~/web.config"))';
 s += 'from @Href("~/Models/Test")';
 alert(s);
 alert('@Url.Action("Index")')
